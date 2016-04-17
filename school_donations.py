290f13261fc1027ab73991e003fb41614ce54e29
annamosullivan <osullivan_anne@hotmail.com>
2016-04-17 21:36:32 +0100

    connect to MongoDB

--- school_donations.py
+++ school_donations.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 MONGODB_HOST = 'ds013260.mlab.com'
 MONGODB_PORT = 13260
 DBS_NAME = 'heroku_6hq98jg7'
-MONGO_URI = 'mongodb://<dbuser>:<dbpassword>@ds013260.mlab.com:13260/heroku_6hq98jg7'
-COLLECTION_NAME = 'projects'
+MONGO_URI = 'mongodb://root:root@ds013260.mlab.com:13260/heroku_6hq98jg7'
+COLLECTION_NAME = 'opendata_projects_clean'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False}
 
@@ -25,7 +25,9 @@ def index():
 
 @app.route("/donorsUSA/projects")
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(MONGO_URI)
+    ##collection = connection[DBS_NAME][COLLECTION_NAME]
+    ##connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=20000)
     json_projects = []

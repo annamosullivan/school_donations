aa211043f9a9b0530182a488274afd3026a62fb2
annamosullivan <osullivan_anne@hotmail.com>
2016-04-19 23:33:46 +0100

    amended school_donations.py to import extra libraries, change mongo uri

--- school_donations.py
+++ school_donations.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
+from urlparse import urlparse
 from bson import json_util
 from bson.json_util import dumps
 
@@ -11,8 +13,9 @@ app = Flask(__name__)
 
 MONGODB_HOST = 'ds013260.mlab.com'
 MONGODB_PORT = 13260
-DBS_NAME = 'heroku_6hq98jg7'
-MONGO_URI = 'mongodb://root:root@ds013260.mlab.com:13260/heroku_6hq98jg7'
+MONGO_URI = os.environ.get('mongodb://heroku_6hq98jg7:2lm9cccqvvl973fo3kqgdhg8aq@ds013260.mlab.com:13260/heroku_6hq98jg7', 'mongodb://localhost:27017/donorsUSA')
+result = urlparse(MONGO_URI)
+DBS_NAME = result.path.split('/')[1]
 COLLECTION_NAME = 'opendata_projects_clean'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False}
@@ -26,10 +29,8 @@ def index():
 @app.route("/donorsUSA/projects")
 def donor_projects():
     connection = MongoClient(MONGO_URI)
-    ##collection = connection[DBS_NAME][COLLECTION_NAME]
-    ##connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=20000)
+    projects = collection.find(projection=FIELDS).sort("_id", 1).limit(20000)
     json_projects = []
     for project in projects:
         json_projects.append(project)
